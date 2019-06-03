1e4ae02b4cc274270067780194397e934f20f832
DESKTOP-IFH7LER\trait <traitblow@gmail.com>
2019-06-03 14:39:40 +0900

    道路スクロール追加

---
+++ Assets/Scripts/Road.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Road : MonoBehaviour
+{
+    [SerializeField] float speed_x;
+    Material _mat;
+    float offset_x;
+    void Start()
+    {
+        _mat = GetComponent<SpriteRenderer>().material;
+        Debug.Log(_mat);
+    }
+    
+    void Update()
+    {
+        offset_x += speed_x;
+        _mat.SetTextureOffset("_MainTex", new Vector2(offset_x, 0));
+    }
+}
