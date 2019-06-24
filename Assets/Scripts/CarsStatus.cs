3de67f7b5cdc3c900e5a2e50f9f79d900852e5ce
佐藤 凌 <ryo@yahoo.co.jp>
2019-06-24 15:16:23 +0900

    自動車データ

---
+++ Assets/Scripts/CarsStatus.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class CarsStatus : MonoBehaviour
+{
+    [Serializable]
+    public class CarData
+    {
+        public string Name;
+        public float Speed;
+        public int JumpNum;
+        public bool GetFlag;
+
+        public CarData(string name, float speed, int jumpNum, bool getFlag)
+        {
+            Name = name;
+            Speed = speed;
+            JumpNum = jumpNum;
+            GetFlag = getFlag;
+        }
+     
+    }
+
+    public static CarData[] cars = new CarData[10];
+    public CarData[] carData = new CarData[10];
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void UpdateCars()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+           CarsStatus.cars[i] = carData[i];
+        }
+    }
+}
