278a4e892dd476934ed291b3209a06c921fe87c9
DESKTOP-IFH7LER\trait <traitblow@gmail.com>
2019-07-22 15:18:42 +0900

    コイン系管理

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+
+    float offset_X = -20.0f;
+
+    void Start()
+    {
+
+    }
+
+    protected void Update()
+    {
+        if (GameSceneManager.player.transform.position.x + offset_X > transform.position.x)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            ShopMain.Money += 1;
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/CoinGroup.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinGroup : MonoBehaviour
+{
+    float offset_X = -20.0f;
+    protected void Update()
+    {
+        if (GameSceneManager.player.transform.position.x + offset_X > transform.position.x)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
